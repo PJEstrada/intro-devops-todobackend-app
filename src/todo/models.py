4ea825a0fc0abf6d95ed83b0e0e013b832e28c1e
Palo <pjestrada006@hotmail.com>
2018-10-28 14:33:44 -0600

    comment

--- src/todo/models.py
+++ src/todo/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here..
 class TodoItem(models.Model):
   title = models.CharField(max_length=256, null=True, blank=True)
   completed = models.BooleanField(blank=True, default=False)
